5240078ab7f422586a646ae278ffcd6338617b80
mashengguang <mashengguang@sunlands.com>
2024-05-16 19:45:36 +0800

    feat:qiankun接入微应用

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './public-path';
 
-createApp(App).mount('#app')
+let instance = null;
+function render(props = {}) {
+  const { container } = props;
+  instance = createApp(App).mount(container ? container.querySelector('#app') : '#app')
+}
+
+// 独立运行时
+if (!window.__POWERED_BY_QIANKUN__) {
+    render();
+  }
+  
+export async function bootstrap() {
+    console.log('[vue] vue app bootstraped');
+  }
+export async function mount(props) {
+    console.log('[vue] props from main framework', props);
+    render(props);
+}
+
+export async function unmount() {
+    instance.$destroy();
+    instance.$el.innerHTML = '';
+    instance = null;
+  }
